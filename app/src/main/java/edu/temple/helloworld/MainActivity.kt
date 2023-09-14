4c1b74c6089909987c034903d54782a632dc859c
Adrien Tolno <tus29878@temple.edu>
2023-09-14 15:49:49 -0400

    Add constraint layour Assign property add even Listener

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -4,23 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var  nameEditText: EditText
+    lateinit var clickMeButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        nameEditText = findViewById(R.id.EditText)
+        clickMeButton = findViewById(R.id.clickMeButton)
+
+
+
+
 
         
         // Respond to button click event per specifications
-
+clickMeButton.setOnClickListener{displayTextView.text = nameEditText.text}
 
     }
 }
